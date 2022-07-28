ed95e2afb9d3992bf46f6361045809586210cb4f
Mohammad <aali361@gmail.com>
2022-07-28 20:14:46 +0430

    add product model

--- prepare2/product/models.py
+++ prepare2/product/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='product/images')
