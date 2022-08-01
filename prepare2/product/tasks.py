c278d50cef1b427270401c7109503a34ad04f9d9
Mohammad <aali361@gmail.com>
2022-08-01 17:25:08 +0430

    minor change

--- prepare2/product/tasks.py
+++ prepare2/product/tasks.py
@@ -3,4 +3,5 @@ from celery import shared_task
 
 @shared_task
 def process_product_image(product_id):
+    # suppose that each product image processing takes about 1 second
     time.sleep(2)
--- prepare2/user/tasks.py
+++ prepare2/user/tasks.py
@@ -3,4 +3,5 @@ from celery import shared_task
 
 @shared_task()
 def send_email(to, subject, body):
+    # suppose that each sending email takes about 1 second
     time.sleep(1)
