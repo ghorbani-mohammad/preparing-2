3b1109745140ccac7b9e9acd34e4d414101336cb
Mohammad <aali361@gmail.com>
2022-08-02 13:00:55 +0430

    add sample periodic task

--- prepare2/prepare2/celery.py
+++ prepare2/prepare2/celery.py
@@ -1,8 +1,16 @@
 import os
 from celery import Celery
 from django.conf import settings
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prepare2.settings")
 app = Celery("prepare2")
 app.config_from_object("django.conf.settings")
-app.autodiscover_tasks(lambda:settings.INSTALLED_APPS)
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+app.conf.beat_schedule = {
+    "periodic_task": {
+        "task": "product.tasks.periodic_task",
+        "schedule": crontab(minute='*'),
+    }
+}
--- prepare2/product/tasks.py
+++ prepare2/product/tasks.py
@@ -1,7 +1,13 @@
 import time
 from celery import shared_task
 
+
 @shared_task
 def process_product_image(product_id):
     # suppose that each product image processing takes about 1 second
     time.sleep(2)
+
+
+@shared_task
+def periodic_task():
+    print('running the periodic task')
