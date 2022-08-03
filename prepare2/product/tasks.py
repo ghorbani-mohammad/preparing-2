dc7cedab431abb8342951fcc5dcf0c62d097eef4
Mohammad <aali361@gmail.com>
2022-08-03 22:15:41 +0430

    minor change

--- prepare2/product/tasks.py
+++ prepare2/product/tasks.py
@@ -1,4 +1,5 @@
 import time
+from . import models
 from celery import shared_task
 
 
@@ -11,3 +12,9 @@ def process_product_image(product_id):
 @shared_task
 def periodic_task():
     print('running the periodic task')
+
+
+@shared_task
+def example_async_task(product_id):
+    product = models.Product.objects.get(pk=product_id)
+    time.sleep(20)
