a9f0096d36cbf1acaeff41f0b5644db1a94ab7ce
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-22 16:33:24 +0330

    minor change

--- prepare2/prepare2/settings.py
+++ prepare2/prepare2/settings.py
@@ -109,10 +109,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
 USE_TZ = True
+USE_I18N = True
+TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'en-us'
 
 
 # Static files (CSS, JavaScript, Images)
