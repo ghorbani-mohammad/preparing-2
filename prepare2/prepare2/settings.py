d19aae13d9a81c8a362dde068e435c37421b755a
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-25 19:03:39 +0330

    minor change

--- prepare2/prepare2/settings.py
+++ prepare2/prepare2/settings.py
@@ -64,9 +64,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'prepare2.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
