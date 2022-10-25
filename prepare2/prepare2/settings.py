c3755dfb8a226e59981f9c13ac6fcfbbcfa4b6d8
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-25 19:08:49 +0330

    minor change

--- prepare2/prepare2/settings.py
+++ prepare2/prepare2/settings.py
@@ -7,9 +7,6 @@ env = environ.Env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
