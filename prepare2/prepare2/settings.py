a962a6f4e48deb14bd680fdeac9e3879f302aa02
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-26 21:16:06 +0330

    minor change

--- prepare2/prepare2/settings.py
+++ prepare2/prepare2/settings.py
@@ -7,7 +7,6 @@ env = environ.Env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
