b9968a492d070cd10e353c70a16943b045e8a5ae
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-26 21:16:21 +0330

    minor change

--- prepare2/prepare2/settings.py
+++ prepare2/prepare2/settings.py
@@ -9,7 +9,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG")
 
 ALLOWED_HOSTS = []
